24d4312df8bf979c4b4e92c0dd3a9b8304ac4a32
DianaSpurza <03022000diana0>
2019-12-12 16:55:42 +0200

    Lab_6: відредаговано travi-build.sh

--- lab_3/scripts/travis-build.sh
+++ lab_3/scripts/travis-build.sh
@@ -3,4 +3,4 @@ set -ev
 nohup pipenv run server > ./ci-build.log &
 pipenv run python monitoring.py &
 sleep 15
-if [[ "$?" == 0 ]]; then exit 0; else exit $?;
+if [ $? -eq 0 ]; then exit 0; else exit $?; fi
